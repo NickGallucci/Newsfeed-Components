bc467bf0101a96e7cc989fd3eb43f4e0169ab626
CryptoN999 <nicholasgallucci4@gmail.com>
2019-09-12 18:41:58 -0700

    stretch problem added animation

--- Article/Article.js
+++ Article/Article.js
@@ -147,6 +147,41 @@ window.addEventListener('load', (e) => {
 		return articleDiv;
 	}
 });
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
 
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
